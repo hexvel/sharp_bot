617ee1102d3059665da1273af0d0165b5eb35846
Another <89122911+hexvel@users.noreply.github.com>
2023-05-20 19:40:39 +0300

    Add files via upload

---
+++ vk/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using VkNet;
+using VkNet.Model;
+namespace VkBot
+{
+    public class MainClassVk
+    {
+        public static void Main()
+        {   
+         Dictionary<string, object> Params(params object[] args)
+         {
+            {''}
+         }; 
+        }
+
+        static async Task<HttpResponseMessage?> GetResponse(string url, Dictionary<string, string> Params)
+        {
+            HttpClient client = new HttpClient();
+
+            try
+            {
+                Uri uri = new Uri(url);
+                FormUrlEncodedContent content = new FormUrlEncodedContent(Params);
+
+                return await client.PostAsync(url, content);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Произошла ошибка:\n", ex.ToString());
+            }
+            finally
+            {
+                client.Dispose();
+            }
+
+            return null;
+        }
+    }
+}
