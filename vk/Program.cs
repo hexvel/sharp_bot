4733a8c311704340a745257eaf3e812254f6e2fc
HEXVEL <dimaabdukarimov1234@gmail.com>
2023-05-20 23:39:58 +0300

    Ну я типа создал функцию для пост запросов к вк апи

--- vk/Program.cs
+++ vk/Program.cs
@@ -1,19 +1,24 @@
 ﻿using System;
-using VkNet;
-using VkNet.Model;
 namespace VkBot
 {
     public class MainClassVk
     {
         public static void Main()
-        {   
-         Dictionary<string, object> Params(params object[] args)
-         {
-            {''}
-         }; 
+        {
+            string token = "vk1.a.UknqVGrk4pDGkY3wovzKAjxKekovf1gqMb302tqWu0IsSM8QtFKKxlL-imyLstU2crwe2rxvRiYnTNCDoHgqRLxoKMzSzKV8ZIu5unO5fR216Wbom4_slpVPeY3Xo5w7g9z4HvinEMblyriZUtjcI98gQJSmQj7_05G6wXwrB5xP6gFhKDpGAofT1UNX3vEpZp-P3IbQlVPW_UvRYfCXlw";
+
+            Dictionary<string, string> Params = new Dictionary<string, string>()
+            {
+                ["user_id"] = "465816400",
+                ["access_token"] = token,
+                ["v"] = "5.131"
+            };
+
+            var answer = GetRequests("https://api.vk.com/method/users.get/", Params).Result;
+            System.Console.WriteLine(answer.Content.ReadAsStringAsync().Result);
         }
 
-        static async Task<HttpResponseMessage?> GetResponse(string url, Dictionary<string, string> Params)
+        public static async Task<HttpResponseMessage> GetRequests(string url, Dictionary<string, string> Params)
         {
             HttpClient client = new HttpClient();
 
@@ -22,18 +27,18 @@ namespace VkBot
                 Uri uri = new Uri(url);
                 FormUrlEncodedContent content = new FormUrlEncodedContent(Params);
 
-                return await client.PostAsync(url, content);
+                return await client.PostAsync(uri, content);
             }
             catch (Exception ex)
             {
-                Console.WriteLine("Произошла ошибка:\n", ex.ToString());
+                System.Console.WriteLine($"Произошла ошибка:{ex}");
             }
             finally
             {
                 client.Dispose();
             }
-
             return null;
         }
+
     }
 }
